5971c5f789e67acecac0fdb336738285c6d35c7b
Scott Jacobson <scojac@upenn.edu>
2024-01-31 19:58:29 -0500

    intial commit

---
+++ force-app/main/default/lwc/screenFlowTabs/screenFlowTabs.js
@@ -0,0 +1,20 @@
+// JS
+import { LightningElement, api } from 'lwc';
+
+export default class TabsetScoped extends LightningElement {
+    // Tab 1 properties
+    @api tab1Label = 'Tab 1';
+    @api tab1FlowName = '';
+
+    // Tab 2 properties
+    @api tab2Label = 'Tab 2';
+    @api tab2FlowName = '';
+
+    // Tab 3 properties
+    @api tab3Label = 'Tab 3';
+    @api tab3FlowName = '';
+
+    handleFlowStatusChange(event) {
+        // Handle flow status changes if needed
+    }
+}
