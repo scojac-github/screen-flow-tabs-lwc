c57f6035e154c282bb094a8f988dca8c99240f99
Scott Jacobson <scojac@upenn.edu>
2024-02-01 10:24:32 -0500

    Enhancements to dynamically create any desired number tabs

--- force-app/main/default/lwc/screenFlowTabs/screenFlowTabs.js
+++ force-app/main/default/lwc/screenFlowTabs/screenFlowTabs.js
@@ -1,17 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
 export default class TabsetScoped extends LightningElement {
-    // Tab 1 properties
-    @api tab1Label = 'Tab 1';
-    @api tab1FlowName = '';
+    @api tabsConfig = '[{"label": "Tab 1", "flowName": ""}, {"label": "Tab 2", "flowName": ""}]';
 
-    // Tab 2 properties
-    @api tab2Label = 'Tab 2';
-    @api tab2FlowName = '';
-
-    // Tab 3 properties
-    @api tab3Label = 'Tab 3';
-    @api tab3FlowName = '';
+    get tabs() {
+        return JSON.parse(this.tabsConfig);
+    }
 
     handleFlowStatusChange(event) {
         // Handle flow status changes if needed
